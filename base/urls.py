f377dc7da948cc87a5e1badd1cb6f1d9867d8d4e
AlexWanyoike <alex.wanyoike12@gmail.com>
2021-06-03 21:15:54 +0300

    Urls.py routes Neighborhood,Business and User Created

--- base/urls.py
+++ base/urls.py
@@ -8,9 +8,7 @@ from . import views
 
 
 urlpatterns=[
-    
     path('',views.base,name = 'base'),
-    path('api-auth/', include('rest_framework.urls')),
     path('api/neighbors/',views.NeighborhoodList.as_view(),name='neighbor'),
     path('api/business/',views.BusinessList.as_view(),name='business'),
     path('api/users/',views.UserList.as_view(),name='users'),
--- neighborhood/settings.py
+++ neighborhood/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    
     'base',
     'bootstrap3',
     'rest_framework',
@@ -130,6 +131,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
