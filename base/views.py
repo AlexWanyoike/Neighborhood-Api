028dd81d8ed22b8bc1a6edc4788fc739c339c6a2
AlexWanyoike <alex.wanyoike12@gmail.com>
2021-06-04 13:40:31 +0300

    Permissions.py Created

--- base/views.py
+++ base/views.py
@@ -6,31 +6,38 @@ from django.http import Http404
 from django.http import response
 from .models import *
 from .serializers import *
+from .permissions import IsAdminOrReadOnly
 # Create your views here.
 class NeighborhoodList(APIView):
+
+  permission_classes = (IsAdminOrReadOnly,)
+
   def get_neighborhood(self, pk):
     try:
         return Neighborhood.objects.get(pk=pk)
     except Neighborhood.DoesNotExist:
         return Http404()
+
   def get(self,request,format=None):
     neighborhood= Neighborhood.objects.all()
     serializers=NeighborhoodSerializer(neighborhood, many=True)
     return Response(serializers.data)
+
   def post(self,request,format=None):
     serializers=NeighborhoodSerializer(data=request.data)
     if serializers.is_valid():
       serializers.save()
       neighborhood=serializers.data
       response = {
-          'data': {
-              'neighborhood': dict(neighborhood),
-              'status': 'success',
-              'message': 'neighborhood created successfully',
-          }
+        'data': {
+            'neighborhood': dict(neighborhood),
+            'status': 'success',
+            'message': 'neighborhood created successfully',
+        }
       }
       return Response(response, status=status.HTTP_200_OK)
     return Response(serializers.errors , status= status.HTTP_400_BAD_REQUEST)
+
   def put(self, request, pk, format=None):
     neighborhood = self.get_neighborhood(pk)
     serializers = NeighborhoodSerializer(neighborhood, request.data)
@@ -47,20 +54,28 @@ class NeighborhoodList(APIView):
       return Response(response)
     else:
       return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
+
   def delete(self, request, pk, format=None):
     neighborhood = self.get_neighborhood(pk)
     neighborhood.delete()
     return Response(status=status.HTTP_204_NO_CONTENT)
+
+  
+
+
 class BusinessList(APIView):
+      
   def get_business(self, pk):
     try:
         return Business.objects.get(pk=pk)
     except Business.DoesNotExist:
         return Http404()
+
   def get(self, request,format=None):
     business=Business.objects.all()
     serializers=BusinessSerializer(business, many=True)
     return Response(serializers.data)
+
   def post(self, request, format=None):
     serializers=BusinessSerializer(data=request.data)
     if serializers.is_valid():
@@ -75,6 +90,7 @@ class BusinessList(APIView):
       }
       return Response(response, status=status.HTTP_200_OK)
     return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
+
   def put(self, request, pk, format=None):
     business = self.get_business(pk)
     serializers = BusinessSerializer(business, request.data)
@@ -91,20 +107,25 @@ class BusinessList(APIView):
       return Response(response)
     else:
       return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
+
   def delete(self, request, pk, format=None):
     business = self.get_business(pk)
     business.delete()
     return Response(status=status.HTTP_204_NO_CONTENT)
+
 class UserList(APIView):
+      
   def get_users(self,pk):
     try:
         return User.objects.get(pk=pk)
     except User.DoesNotExist:
         raise Http404()
+
   def get(self,request,format=None):
     users=User.objects.all()
     serializers=UserSerializer(users, many=True)
     return Response(serializers.data)
+
   def post(self, request, format=None):
     serializers=UserSerializer(data=request.data)
     if serializers.is_valid():
@@ -119,6 +140,7 @@ class UserList(APIView):
       }
       return Response(response, status=status.HTTP_200_OK)
     return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
+
   def put(self,request,pk,format=None):
     users=self.get_users(pk)
     serializers=UserSerializer(users,request.data)
@@ -135,6 +157,7 @@ class UserList(APIView):
       return Response(response, status=status.HTTP_200_OK)
     else:
       return Response(serializers.errors,status=status.HTTP_400_BAD_REQUEST)
+
   def delete(self,request,pk,format=None):
     users=self.get_users(pk)
     users.delete()
--- neighborhood/urls.py
+++ neighborhood/urls.py
@@ -22,5 +22,6 @@ from rest_framework.authtoken.views import obtain_auth_token
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('base.urls')),
+    path('api-token-auth/', obtain_auth_token)
     
 ]
