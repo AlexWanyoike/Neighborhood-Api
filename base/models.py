11a4f3a0b3cb494bdf8d44c3b2dc6c68f111e3cd
AlexWanyoike <alex.wanyoike12@gmail.com>
2021-06-05 03:14:23 +0300

    Admin.py Created- Register Site added

--- base/models.py
+++ base/models.py
@@ -1,13 +1,17 @@
 from django.db import models
-# Create your models here.
 from django.db import models
+import datetime as dt
+from django.contrib.auth.models import User 
+from django.db.models.base import Model
 # from django.contrib.auth.models import User
 # Create your models here.
 
 class Neighborhood(models.Model):
     name = models.CharField(max_length=30)
     location = models.CharField(max_length=50) 
-    
+    occupants_count = models.FloatField(min_length=100)
+    description = models.TextField(min_length= 6)
+
     def __str__(self):
         return self.name
 
@@ -28,8 +32,10 @@ class Neighborhood(models.Model):
 
 class User(models.Model):
       
+    user = models.OneToOneField(User, on_delete=models.CASCADE ) 
     name = models.CharField(max_length=50)
     email = models. EmailField()
+    profile_pic = models.ImageField(upload_to='media/')
     neighborhood = models.ForeignKey(Neighborhood, on_delete=models.CASCADE)
 
     def __str__(self):
@@ -44,6 +50,7 @@ class User(models.Model):
 class Business(models.Model):
     name=models.CharField(max_length=50)
     user=models.ForeignKey(User,on_delete=models.CASCADE)
+    description = models.TextField(min_length=10, max_length=300)
     email=models.EmailField()
     neighborhood=models.ForeignKey(Neighborhood,on_delete=models.CASCADE)
 
@@ -64,3 +71,24 @@ class Business(models.Model):
     def update_business(cls, id, name):
       update = cls.objects.filter(id=id).update(name=name)
       return update 
+
+class Post(model.models):
+      
+  user = models.ForeignKey(User,on_delete=models.CASCADE,related_name='posts',null=true)
+  title = models.CharField(max_length=160)
+  content = models.TextField()
+  date_posted = models.DateTimeField(auto_now_add=True)
+  neighborhood = models.ForeignKey(Neighborhood,on_delete=models.CASCADE, null=True)
+
+  def __str__(self):
+    return self.title
+
+  def save_image(self):
+    self.save()
+
+  def delete_image(self):
+    self.delete()
+
+  def update_content(self , new_content):
+    self.content = new_content
+    self.save()
--- base/tests.py
+++ base/tests.py
@@ -1,3 +1,3 @@
 from django.test import TestCase
-
+from .models import Neighborhood, User, Post, Business
 # Create your tests here.
