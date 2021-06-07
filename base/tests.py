96f6fbbb98f41707b54524412c9d88132cdf6e08
AlexWanyoike <alex.wanyoike12@gmail.com>
2021-06-07 23:24:33 +0300

    Serilializer Added and Post Model Added

--- base/tests.py
+++ base/tests.py
@@ -8,23 +8,13 @@ from .models import Profile , Post, Comment
 from django.contrib.auth.models import User
 # Create your tests here.
 
-class NeighborhoodTestClass(TestCase):
-
+class ProfileTestClass(TestCase):
+    
     def setUp(self):
-        self.user = User(id=1, user='alex')
+        self.user = User(id=1, username='alex', password='1234')
         self.user.save()
 
     def test_instance(self):
         self.assertTrue(isinstance(self.user, User))
 
-    def test_save_user(self):
-        self.user.save()
-
-    def test_delete_user(self):
-        self.user.delete()
-
-
-
-
    
-    
